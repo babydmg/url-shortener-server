5e2bbec619a7351ee6f0954a7295fad7326494e6
babydmg <126503300+babydmg@users.noreply.github.com>
2024-02-26 13:35:25 +0500

    hello

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const dbConnect = require('./dbConnect');
 const URLModel = require('./models/URL.model');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 dbConnect();
 
